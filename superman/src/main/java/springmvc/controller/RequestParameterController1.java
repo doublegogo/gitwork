58732493c486ab01e8ac7e7512e641b639ff7098
lipeng <beikaobei303@163.com>
2017-07-20 01:53:39 +0800

    superman 新项目

---
+++ superman/src/main/java/springmvc/controller/RequestParameterController1.java
@@ -0,0 +1,23 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/parameter1")                                      //①处理器的通用映射前缀
+public class RequestParameterController1 {
+    //②进行类级别的@RequestMapping窄化
+    @RequestMapping(params="create", method=RequestMethod.GET)
+//    @RequestMapping(params="!create", method=RequestMethod.GET)//进行类级别的@RequestMapping窄化
+    public String showForm() {
+        System.out.println("===============showForm");
+        return "parameter/create";
+    }
+    //③进行类级别的@RequestMapping窄化
+    @RequestMapping(params="create", method= RequestMethod.POST)
+    public String submit() {
+        System.out.println("================submit");
+        return "redirect:/success";
+    }
+}
